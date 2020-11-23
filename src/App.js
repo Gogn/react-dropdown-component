e3f1ad686dfc5cddec83088a46853baf16c3d7c5
dero <106393@gmail.com>
2020-11-23 16:04:07 +0400

    initial

--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import logo from './logo.svg';
+import React from "react";
+import DropDown from "./dropDown/DropDown";
 
 function App() {
+
+  const data = {
+    stackOne: [
+      {id: 121, name: 'Bitcon', symbol: 'BTC', currency: '$', currentPrice: '12773.11', dayChange: ((Math.random()*100).toFixed(2))},
+      {id: 123, name: 'Ripple', symbol: 'XRP', currency: '$', currentPrice: '0.29', dayChange: ((Math.random()*100).toFixed(2))},
+      {id: 124, name: 'Ethereum', symbol: 'ETH', currency: '$', currentPrice: '508.20', dayChange: ((Math.random()*100).toFixed(2))},
+      {id: 125, name: 'Litecoin', symbol: 'LTC', currency: '$', currentPrice: '82.09', dayChange: ((Math.random()*100).toFixed(2))},
+      {id: 126, name: 'Bitcoin Cash / BCC', symbol: 'BCC', currency: '$', currentPrice: '256.10', dayChange: ((Math.random()*100).toFixed(2))},
+      {id: 127, name: 'EOS', symbol: 'EOS', currency: '$', currentPrice: '2.63', dayChange: ((Math.random()*100).toFixed(2))},
+      {id: 128, name: 'Binance Coin', symbol: 'BNB', currency: '$', currentPrice: '29.00', dayChange: ((Math.random()*100).toFixed(2))},
+      {id: 129, name: 'Tether', symbol: 'USDT', currency: '$', currentPrice: '1.00', dayChange: ((Math.random()*100).toFixed(2))}
+  ]
+  }
+
+  const preloadFunc = (data) => {
+    return data.stackOne.filter((x)=> x.id >= 1 )
+  }
+
+  const onChangeCallback = async(data) => {
+    let effect = new Promise((resolve, reject) => {
+      setTimeout(() => resolve('Done'), 1000)
+    })
+
+    let res = await effect
+    console.log(res)
+    console.log('items: ',data)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
+        <DropDown
+          multiSelect={true}
+          placeholder='Поиск валют...'
+          data={data}
+          onChangeCallback={(data) => {onChangeCallback(data)}}
+          preloadFunc={preloadFunc}
+        />
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
---
+++ src/dropDown/DropDown.js
@@ -0,0 +1,177 @@
+import React, { useEffect, useState } from "react";
+import './dropDown.css'
+
+const DropDown = ({ data, multiSelect, placeholder, preloadFunc, onChangeCallback }) => {
+  const [selectedItems, setSelectedItems] = useState([])
+  const [showList, setShowList] = useState(true)
+  const [dataArr, setDataArr] = useState([])
+  const [search, setSearch] = useState('')
+  const [isLoading, setIsLoading] = useState(true)
+
+  // Load data
+  useEffect(() => {
+
+    let objToArr = () => {
+      setIsLoading(true)
+      if (!Array.isArray(data)) {
+        for (let key in data) {
+          if (!Array.isArray(data[key])) {
+            objToArr(data[key])
+          } else {
+            setDataArr(data[key])
+          }
+        }
+      }
+    }
+
+    objToArr()
+    if (preloadFunc) {
+      setDataArr(preloadFunc(data))
+    }
+    setIsLoading(false)
+  }, [data])
+
+  // onChangeCallback
+  useEffect(() => {
+    !isLoading && onChangeCallback(selectedItems)
+  }, [selectedItems])
+
+  const setItem = (item) => {
+    console.log('item:', item)
+    console.log('selectedItems.includes(item)', selectedItems.includes(item))
+    // Remove
+    if (selectedItems.includes(item)) {
+      setSelectedItems(selectedItems.filter(i => i !== item))
+    } else {
+      // Add
+      if (multiSelect) {
+        setSelectedItems([...selectedItems, item])
+      } else {
+        if (selectedItems.length === 0) {
+          setSelectedItems([...selectedItems, item])
+        }
+      }
+    }
+  }
+
+  const getSelectedItems = () => {
+    console.log('getSelectedItems')
+    console.log('selectedItems', selectedItems)
+    return selectedItems.map((item, index) => {
+      let res = []
+      for (let x of dataArr) {
+        if (x.id === item) {
+          res.push(
+            <div key={index + item} className={'selected-items__item'}>
+              <div>
+                {x.name}
+              </div>
+              <button className={'selected-item__button'} onClick={() => setItem(item)}> X</button>
+            </div>
+          )
+        }
+      }
+      return res
+
+    })
+  }
+
+  const getItems = () => {
+
+    const icons = [
+      { symbol: 'BTC', icon: 'B' },
+      { symbol: 'XRP', icon: 'X' },
+      { symbol: 'ETH', icon: 'E' },
+      { symbol: 'LTC', icon: 'L' },
+      { symbol: 'BCC', icon: 'B' },
+      { symbol: 'EOS', icon: 'E' },
+      { symbol: 'BNB', icon: 'B' },
+      { symbol: 'USDT', icon: 'T' },
+    ]
+
+    const getSymbol = (symbol) => {
+      for (let i of icons) {
+        if (i.symbol === symbol) return i.icon
+      }
+    }
+
+    return dataArr.map((item, i) => {
+      let res = []
+      if (item.name.toLowerCase().includes(search) || search === '') res.push(
+        <div className={'list-item'}>
+
+          <div className={'list-item__position'}>
+
+            <div className={'list-item__icon'}>
+              {getSymbol(item.symbol)}
+            </div>
+
+            <div>
+              <div className={'list-item__position'}>
+                <div className={'list-item__name'}>
+                  {item.name}
+                </div>
+                <div className={'list-item__currency'}>
+                  {item.currentPrice}{item.currency}
+                </div>
+              </div>
+
+              <div className={'list-item__change'}>
+                {item.dayChange}%
+              </div>
+            </div>
+
+          </div>
+
+          <button
+            className={'list__button ' + (selectedItems.includes(item.id) ? 'list__button--selected' : 'list__button--unselected')}
+            onClick={() => setItem(item.id)}
+          >
+            L
+          </button>
+
+        </div>
+      )
+      return res
+    })
+
+  }
+
+  return (
+    <div className={"dropDown-container"}>
+
+      <div className={'selectedItems-container flex-between flex-row'}>
+
+        <div className={'flex1'}>
+          <div className={'selected-items'}>
+            {getSelectedItems()}
+          </div>
+          <input
+            className={'selectedItems-container__search'}
+            type="text"
+            placeholder={placeholder}
+            onChange={(event) => setSearch(event.target.value)}
+          />
+        </div>
+        <div>
+          <button
+            className={'selectedItems-container__button'}
+            onClick={() => setShowList(!showList)}
+          >
+            {showList === true ? '^' : 'v'}
+          </button>
+        </div>
+
+      </div>
+
+      {showList === true &&
+      <div className={'list'}>
+        {getItems()}
+      </div>
+      }
+
+    </div>
+  )
+}
+
+export default DropDown
